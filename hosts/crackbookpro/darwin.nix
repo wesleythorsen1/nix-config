024ba6788aaca1a19973a038ae09ce5497f96fb2
wesleythorsen1 <wesley.thorsen@gmail.com>
2025-05-16 14:19:51 -0400

    update unstable input, reorg darwin.nix

--- hosts/crackbookpro/darwin.nix
+++ hosts/crackbookpro/darwin.nix
@@ -5,73 +5,95 @@
 }:
 
 {
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.overlays = overlays;
   nixpkgs.config.allowUnfree = true;
 
   networking.hostName = "crackbookpro";
 
-  users.users.wes = {
-    home  = "/Users/wes";
-    shell = pkgs.zsh;
+  system = {
+    stateVersion = 5;
+
+    defaults = {
+      NSGlobalDomain = {
+        AppleShowAllExtensions    = true;  # Show file extensions
+      };
+
+      dock = {
+        autohide                  = true;  # Auto-hide the Dock
+        mru-spaces                = false; # Don’t “Automatically rearrange Spaces based on most recent use”
+      };
+
+      WindowManager = {
+        GloballyEnabled           = true;  # Enable "Stage Manager"
+      };
+    };
+
+    keyboard = {
+      enableKeyMapping            = true;  # Turn on nix-darwin’s key-mapping support
+      # swapLeftCommandAndLeftAlt = true;  # Swap the left Command (⌘) and left Option (⌥) keys
+      # remapCapsLockToControl    = true;  # Remap Caps Lock to Control
+      # swapLeftCtrlAndFn         = false; # Swap left Control and Fn/Globe
+    };
+
+    activationScripts = {
+      # Apply any changed defaults immediately (no login/logout)
+      postUserActivation.text = ''
+        /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+      '';
+
+      # postUserActivation.text = ''
+      #   profiles install -type configuration -path ${config.environment.etc."tcc-pppc.mobileconfig".source}
+      # '';
+    };
   };
 
-  services.nix-daemon.enable = true; # Ensure the Nix daemon runs on startup
-
-  system.stateVersion = 5;
-
-  system.defaults = {
-    NSGlobalDomain.AppleShowAllExtensions = true;  # Show file extensions
-    dock.autohide                         = true;  # Auto-hide the Dock
-    dock.mru-spaces                       = false; # Don’t “Automatically rearrange Spaces based on most recent use”
+  users.users = {
+    wes = {
+      home  = "/Users/wes";
+      shell = pkgs.zsh;
+    };
   };
 
-  system.keyboard = {
-    enableKeyMapping          = true;  # Turn on nix-darwin’s key-mapping support
-    # swapLeftCommandAndLeftAlt = true;  # Swap the left Command (⌘) and left Option (⌥) keys
-    # remapCapsLockToControl    = true;  # Remap Caps Lock to Control
-    # swapLeftCtrlAndFn         = false; # Swap left Control and Fn/Globe
+  services = {
+    nix-daemon.enable             = true; # Ensure the Nix daemon runs on startup
+
+    # launchd = {
+    #   userAgents."remap-command-control" = {
+    #     enable = true;
+    #     runAtLoad = true;
+    #     serviceConfig = {
+    #       Label            = "remap-command-control";
+    #       Program          = "${pkgs.hidutil}/bin/hidutil";
+    #       ProgramArguments = [
+    #         "property" "--set" ''
+    #           {
+    #             "UserKeyMapping": [
+    #               # 0x7000000E0 = Left Control; 0x7000000E3 = Left Command
+    #               {"HIDKeyboardModifierMappingSrc":0x7000000E0,"HIDKeyboardModifierMappingDst":0x7000000E3},
+    #               {"HIDKeyboardModifierMappingSrc":0x7000000E3,"HIDKeyboardModifierMappingDst":0x7000000E0}
+    #             ]
+    #           }
+    #         ''
+    #       ];
+    #     };
+    #   };
+    # };
   };
 
-  # services.launchd.userAgents."remap-command-control" = {
-  #   enable = true;
-  #   runAtLoad = true;
-  #   serviceConfig = {
-  #     Label            = "remap-command-control";
-  #     Program          = "${pkgs.hidutil}/bin/hidutil";
-  #     ProgramArguments = [
-  #       "property" "--set" ''
-  #         {
-  #           "UserKeyMapping": [
-  #             # 0x7000000E0 = Left Control; 0x7000000E3 = Left Command
-  #             {"HIDKeyboardModifierMappingSrc":0x7000000E0,"HIDKeyboardModifierMappingDst":0x7000000E3},
-  #             {"HIDKeyboardModifierMappingSrc":0x7000000E3,"HIDKeyboardModifierMappingDst":0x7000000E0}
-  #           ]
-  #         }
-  #       ''
-  #     ];
-  #   };
-  # };
+  environment = {
+    # etc = {
+    #   "tcc-pppc.mobileconfig" = {
+    #     source = ./tcc-pppc.mobileconfig;
+    #     mode   = "0644";
+    #   };
+    # };
 
-  # environment.etc."tcc-pppc.mobileconfig" = {
-  #   source = ./tcc-pppc.mobileconfig;
-  #   mode   = "0644";
-  # };
-
-  # system.activationScripts.postUserActivation.text = ''
-  #   profiles install -type configuration -path ${config.environment.etc."tcc-pppc.mobileconfig".source}
-  # '';
-
-  # Apply any changed defaults immediately (no login/logout)
-  system.activationScripts.postUserActivation.text = ''
-    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-  '';
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    systemPackages = with pkgs; [
+      git
+      coreutils
+    ];
+  };
 
   programs.zsh.enable = true; # Enable zsh system-wide
-
-  environment.systemPackages = with pkgs; [
-    git
-    coreutils
-  ];
 }
