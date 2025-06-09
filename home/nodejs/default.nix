5b068670d6cd2d88f3a99cecd43f0562021fb5d7
wesleythorsen1 <wesley.thorsen@gmail.com>
2025-06-09 12:32:48 -0400

    feat(home)!: update shell aliases to include opencommit and remove aicommits for better tooling integration
    BREAKING CHANGE: remove aicommits cli tool

--- home/nodejs/default.nix
+++ home/nodejs/default.nix
@@ -13,8 +13,14 @@
         shellAliases = {
             serverless = "npx serverless@3";
             sls = "serverless";
-            aicommits = "npx aicommits";
-            aic = "aicommits";
+            # aicommits = "npx aicommits";
+            # aic = "aicommits";
+            opencommit = "npx opencommit";
+            oco = "opencommit";
         };
+
+        # oco config set OCO_API_KEY=<OPENAI_API_KEY>
+        # oco config set OCO_PROMPT_MODULE=@commitlint
+        # oco config set OCO_PROMPT_MODULE=conventional-commit
     };
 }
