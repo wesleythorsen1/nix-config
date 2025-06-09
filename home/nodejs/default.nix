02df6ab96a5eeb2ca2aaeb9b62b6759f0c9bba44
wesleythorsen1 <wesley.thorsen@gmail.com>
2025-06-09 12:32:48 -0400

    feat(home): add opencommit npx shell alias

--- home/nodejs/default.nix
+++ home/nodejs/default.nix
@@ -13,6 +13,12 @@
         shellAliases = {
             serverless = "npx serverless@3";
             sls = "serverless";
+            opencommit = "npx opencommit";
+            oco = "opencommit";
         };
+
+        # oco config set OCO_API_KEY=<OPENAI_API_KEY>
+        # oco config set OCO_PROMPT_MODULE=@commitlint
+        # oco config set OCO_PROMPT_MODULE=conventional-commit
     };
 }
