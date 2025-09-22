3227e08da72b1d2973ee0912bc507c02eee4bbb2
wesleythorsen1 <wesley.thorsen@gmail.com>
2025-09-22 11:25:16 -0400

    feat: change neovim from spaces to tabs

--- home/shell/nvim/config/settings.lua
+++ home/shell/nvim/config/settings.lua
@@ -8,7 +8,7 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 
 vim.opt.smartindent = true
 
